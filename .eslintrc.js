421f76377d8d4fab5536c6ee2bca677761babb63
Netsl <vanya6537@mail.ru>
2020-09-13 18:30:02 +0400

    Added webpack config, jest, eslint, prettier, express server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,37 @@
 module.exports = {
   extends: [
-    'airbnb',
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:import/typescript",
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-    ecmaVersion: 2020,
+    project: "tsconfig.json",
+    sourceType: "module",
     ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
+      jsx: true,
     },
+    ecmaVersion: 2018,
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
 
   // Fine tune rules
   settings: {
     react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
       },
     },
   },
   rules: {
-    'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
-    '@typescript-eslint/no-var-requires': 0,
-    'jsx-a11y/label-has-associated-control': 0,
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 0,
-    'import/prefer-default-export': 'off',
-    'import/no-default-export': 'error',
-    'import/no-unresolved': 0,
-    'no-underscore-dangle': ['error', { allow: ['__typename'] }],
-    '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-      mjs: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-    }],
+    "jsx-a11y/label-has-associated-control": 0,
+    "import/prefer-default-export": "off",
+    "no-underscore-dangle": ["error", {allow: ["__typename"]}],
+    "react/jsx-filename-extension": [1, {extensions: [".tsx", ".jsx"]}],
   },
 };
---
+++ configs/webpack/common.js
@@ -0,0 +1,60 @@
+// shared config (dev and prod)
+const path = require("path");
+const {CheckerPlugin} = require("awesome-typescript-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  context: path.resolve(__dirname, "../../src"),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader", "source-map-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "awesome-typescript-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {loader: "css-loader", options: {importLoaders: 1}},
+        ],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: [
+          "style-loader",
+          {loader: "css-loader", options: {importLoaders: 1}},
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          "file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]",
+          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({template: "index.html.ejs"}),
+  ],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  performance: {
+    // hints: false,
+  },
+};
