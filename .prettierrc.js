459759270fec501c9ba110a5073d0d8c1f12d564
Netsl <vanya6537@mail.ru>
2020-09-20 04:51:44 +0400

    Eslint fixes, prettier renewal

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,18 @@
+const presets = [
+    ['@babel/preset-typescript'],
+    ['@babel/preset-react'],
+];
+const plugins = [
+    ['@babel/plugin-transform-runtime'],
+    ['@babel/plugin-syntax-dynamic-import'],
+    ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+    ['@babel/plugin-proposal-class-properties'],
+    // ['@babel/plugin-transform-modules-commonjs'],
+    ['@babel/plugin-transform-arrow-functions'],
+    ['@babel/plugin-transform-object-assign'],
+];
 module.exports = {
-    semi: true,
-    trailingComma: "all",
-};
+        presets,
+        plugins,
+        ignore: [/\/node_modules\//],
+}
